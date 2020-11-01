531151d09a132809e7bdea6bdd9ace472a1ffee4
gautrais <francois@gautrais.eu>
2020-11-02 00:44:17 +0100

    Port

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 export PYTHONPATH="$PYTHONPATH:../libs/python"
-python3  server.py 8081 > covid.log
+python3  server.py 8081 > covid.log &
+
